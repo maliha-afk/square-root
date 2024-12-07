b402e3703158a7fb08f3e50c6c2cc78601ff98f8
maliha-afk <24amaliha@gmail.com>
2024-12-07 20:32:25 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import math
+
+print("welcome to square root")
+Y=1
+N=2
+
+num=int(input("enter your number: "))
+
+while True:
+ print(math.sqrt(num))
+ numb=int(input(f"enter your number or do u want to stop? ({Y}(Y)/{N}(N)): "))
+ 
+ if Y==True:
+  break
+
+ elif N==True:
+  pass 
+
+  
+ 
+
+ 
